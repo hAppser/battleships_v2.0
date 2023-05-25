71232d41452d3db952ba9ef8fe5faf9323087fe5
hAppser <vip313z@ukr.net>
2023-05-25 17:04:40 +0300

    fixing git problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState } from "react";
 import MainMenu from "./components/MainMenu/MainMenu";
 import Login from "./components/Login/Login";
@@ -27,5 +28,31 @@ const App: React.FC = () => {
     </div>
   );
 };
+=======
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+>>>>>>> 0503eff (Initialize project using Create React App)
 
 export default App;
